91609eed8a11dd446c23540ae902b8b627681311
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2023-07-17 16:44:56 -0300

    Add javascript a function menuShow()

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+function menuShow() {
+  let menuMobile = document.querySelector('.mobile-menu');
+  if (menuMobile.classList.contains('open')){
+    menuMobile.classList.remove('open');
+  } else {
+    menuMobile.classList.add('open');
+  }
+};
+
+
 const prevButton = document.querySelector('.prev-button');
 const nextButton = document.querySelector('.next-button');
 const cardsContainer = document.querySelector('.cards-container');
