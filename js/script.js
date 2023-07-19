07553b83cdbf8613e7ab32aa484e13fd8e436af3
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2023-07-19 14:55:58 -0300

    Menu responsivo ajustado

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ function menuShow() {
   if (menuMobile.classList.contains('open')){
     menuMobile.classList.remove('open');
   } else {
-    menuMobile.classList.add('open');
+    menuMobile.classList.add('open')
   }
 };
 
